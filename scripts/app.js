dfa30b891bbacfbe7c3ecc1f0c7951b361e58d64
Evgenii Kucheriavyi <e.kucheriavii@gmail.com>
2020-05-25 14:24:11 +0300

    Added comments and readme file

--- scripts/app.js
+++ scripts/app.js
@@ -1,25 +1,33 @@
+//Создаём переменную, в которой будут находиться наши превью
 let videos;
 
+//Запускаем функцию, которая найдёт все видео на странице и добавит обработчики событий
 Init();
 
 function Init()
 {
+	//Получаем видео
 	videos = document.getElementsByClassName("item__thumb");
 
 	for(var i = 0; i < videos.length; i++)
 	{
-		//videos[i].volume = 0;
+		//Добавляем обработчик события наведения мыши
 		videos[i].addEventListener("mouseenter", function(e) { MouseEnter(e.target); });
+
+		//Добавляем обработчик события выходу курсора за границы элемента
 		videos[i].addEventListener("mouseleave", function(e) { MouseLeave(e.target); });
 	}
 
 }
 
+//Если мышь наведена, просто запускаем ролик
 function MouseEnter(e)
 {
 	e.play();
 }
 
+//Если курсор выходит за пределы элемента, то мы сначала ставим видео на паузу
+//А потом перезагружаем его, чтобы отобразился постер
 function MouseLeave(e)
 {
 	e.pause();
